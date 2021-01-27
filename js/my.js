304bba3248f84273f3978bb867613d923286027f
hmd-hmd <hmd-hmd@gmail.com>
2021-01-27 11:24:32 +0700

    added new items and howto.html

--- js/my.js
+++ js/my.js
@@ -2,7 +2,12 @@ document.getElementById("btnBuy").addEventListener("click", function () {
     document.getElementById("payment").style.display = "block" ;
 });
 
-function paySuccess() {
+function sent() {
+    document.getElementById("sentMessage").style.display = "block" ;
+    console.log("asuu")
+};
+
+function buy() {
     document.getElementById("payStatus").innerHTML = "Status Pembayaran";
     document.getElementById("paymentMethod").style.display ="none";
     document.getElementById("paySuccess").style.display = "block";
